4a5866557f56dfee95a4da82d3ed57324285ef5e
swbonjour <k.koprov@dppm.pro>
2024-09-15 16:11:05 +0300

    LINKED-LIST: Singly Linked List

---
+++ src/LinkedList/SinglyLinkedList.ts
@@ -0,0 +1,11 @@
+export class SinglyLinkedListNode {
+  public data: number;
+  public next?: SinglyLinkedListNode;
+
+  constructor(data: number) {
+    this.data = data;
+    this.next = undefined;
+  }
+}
+
+export class SinglyLinkedListOperations {}
