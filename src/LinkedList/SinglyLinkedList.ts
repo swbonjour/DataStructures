fa53eb1e335bf95f3404884229e577b627a67923
swbonjour <k.koprov@dppm.pro>
2024-09-21 17:46:53 +0300

    SLL: op

--- src/LinkedList/SinglyLinkedList.ts
+++ src/LinkedList/SinglyLinkedList.ts
@@ -25,11 +25,15 @@ export class SinglyLinkedListOperations {
   public traverse(head: SinglyLinkedListNode<any>) {
     let node: SinglyLinkedListNode<any> | undefined = head;
 
+    const res: any[] = [];
+
     while (node) {
-      console.log(node.data);
+      res.push(node.data);
 
       node = node.next;
     }
+
+    console.log(res);
   }
 
   public search(head: SinglyLinkedListNode<any>, value: any) {
@@ -44,5 +48,101 @@ export class SinglyLinkedListOperations {
     }
 
     return null;
+  }
+
+  public length(head: SinglyLinkedListNode<any>) {
+    let node: SinglyLinkedListNode<any> | undefined = head;
+
+    let length = 0;
+
+    while (node) {
+      length++;
+
+      node = node.next;
+    }
+
+    return length;
+  }
+
+  public insertMiddle(
+    head: SinglyLinkedListNode<any>,
+    value: any,
+    pos: number
+  ) {
+    let node: SinglyLinkedListNode<any> | undefined = head;
+
+    let index = 0;
+    while (node) {
+      if (index === pos) {
+        const newNode = new SinglyLinkedListNode(value);
+
+        const rightNode = node.next;
+
+        node.next = newNode;
+
+        newNode.next = rightNode;
+
+        return;
+      }
+
+      node = node.next;
+      index++;
+    }
+  }
+
+  public insertEnd(head: SinglyLinkedListNode<any>, value: any) {
+    let node: SinglyLinkedListNode<any> = head;
+
+    while (node) {
+      if (!node.next) {
+        break;
+      }
+
+      node = node.next;
+    }
+
+    const newNode = new SinglyLinkedListNode(value);
+
+    node.next = newNode;
+  }
+
+  public insertStart(head: SinglyLinkedListNode<any>, value: any) {
+    const newNode = new SinglyLinkedListNode(value);
+
+    newNode.next = head;
+
+    return newNode;
+  }
+
+  public deleteMiddle(head: SinglyLinkedListNode<any>, pos: number) {
+    let node: SinglyLinkedListNode<any> | undefined = head;
+
+    let index = 0;
+    while (node) {
+      if (index === pos - 1) {
+        node.next = node.next?.next;
+        return;
+      }
+
+      node = node.next;
+      index++;
+    }
+  }
+
+  public deleteEnd(head: SinglyLinkedListNode<any>) {
+    let node: SinglyLinkedListNode<any> | undefined = head;
+
+    while (node) {
+      if (!node.next?.next) {
+        node.next = undefined;
+        return;
+      }
+
+      node = node.next;
+    }
+  }
+
+  public deleteStart(head: SinglyLinkedListNode<any>) {
+    return head.next;
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { SinglyLinkedListOperations } from "./LinkedList/SinglyLinkedList";
 
+const breakLine = () => console.log("---------");
+
 const SLLOp = new SinglyLinkedListOperations();
 
 const SLLnHead = SLLOp.createNode(5);
@@ -9,4 +11,44 @@ SLLOp.setLink(SLLnHead, SLLnSecond);
 
 SLLOp.traverse(SLLnHead);
 
-console.log(SLLOp.search(SLLnHead, 5));
+breakLine();
+
+// console.log(SLLOp.search(SLLnHead, 5));
+
+// console.log(SLLOp.length(SLLnHead));
+
+SLLOp.insertMiddle(SLLnHead, 6, 1);
+
+SLLOp.traverse(SLLnHead);
+
+breakLine();
+
+SLLOp.insertEnd(SLLnHead, 8);
+
+SLLOp.traverse(SLLnHead);
+
+breakLine();
+
+const newHead = SLLOp.insertStart(SLLnHead, 2);
+
+SLLOp.traverse(newHead);
+
+breakLine();
+
+SLLOp.deleteMiddle(newHead, 3);
+
+SLLOp.traverse(newHead);
+
+breakLine();
+
+SLLOp.deleteEnd(newHead);
+
+SLLOp.traverse(newHead);
+
+breakLine();
+
+const newNewHead = SLLOp.deleteStart(newHead);
+
+if (newNewHead) {
+  SLLOp.traverse(newNewHead);
+}
